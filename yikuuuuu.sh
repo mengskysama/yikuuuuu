6b910ee8a85404964a1351e21ef2d9f3df3a4d2c
mengskysama <mengskysama@gmail.com>
2016-09-02 22:11:40 -0700

    add more logging

--- yikuuuuu.sh
+++ yikuuuuu.sh
@@ -4,12 +4,12 @@ password='000000'
 do_login() {
     t="1111111111111"
     data='passport='$username'&password='$password'&captcha=&remember=1&callback=logincallback_'$t'&from=http%3A%2F%2Flogin.youku.com%2F@@@@&wintype=page'
-    result=$(curl -A "User-Agent: Mozilla/4.0" -c youku.cookie --location --data $data http://login.youku.com/user/login_submit/ 2>/dev/null)
+    result=$(curl -A "User-Agent: Mozilla/4.0" -c /tmp/youku.cookie --location --data $data http://login.youku.com/user/login_submit/ 2>/dev/null)
     echo $result
 }
 
 do_speed_up() {
-    result=$(curl -A "User-Agent: Mozilla/4.0" -X POST -b youku.cookie -d '' 'http://vip.youku.com/?c=ajax&a=ajax_do_speed_up' 2>/dev/null)
+    result=$(curl -A "User-Agent: Mozilla/4.0" -X POST -b /tmp/youku.cookie -d '' 'http://vip.youku.com/?c=ajax&a=ajax_do_speed_up' 2>/dev/null)
     echo $result
 }
 
@@ -17,6 +17,7 @@ result=`do_speed_up`
 
 echo "$result" |grep -q "20011"
 if [ $? -eq 0 ]; then
+    echo `date +%Y-%m-%d-%H:%M:%S`" 重新登录"
     result=`do_login`
     echo "$result" |grep -q "len-2"
     if [ $? -eq 0 ]; then
@@ -35,16 +36,22 @@ result=`do_speed_up`
 echo "$result" |grep -q "20021"
 if [ $? -eq 0 ]; then
     echo `date +%Y-%m-%d-%H:%M:%S`" 已经提速"
+    exit
 fi
 
 echo "$result" |grep -q "20011"
 if [ $? -eq 0 ]; then
     echo `date +%Y-%m-%d-%H:%M:%S`" 登录喵喵喵?"
     echo `date +%Y-%m-%d-%H:%M:%S`" 登录喵喵喵?" > /tmp/yiku.status
+    exit
 fi
 
 echo "$result" |grep -q "20000"
 if [ $? -eq 0 ]; then
     echo `date +%Y-%m-%d-%H:%M:%S`" 提速成功"
     echo `date +%Y-%m-%d-%H:%M:%S`" 提速成功" > /tmp/yiku.status
+    exit
 fi
+
+echo "其他错误"
+echo $result
